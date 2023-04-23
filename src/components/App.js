7d731f7e7acd562e974d681fb7e1abc36fc8be40
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-23 21:29:15 +0300

    added the theme color change

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
+  // const [mode_type, setMode] = useState("false")
+  var [click_button, setClick] =useState(0)
+
+  function changeMode() {
+    setClick(click_button ++);
+
+  }
+
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = ( click_button%2 === 0 ) ? "App dark" : "App light"
+  console.log(appClass)
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={changeMode}>{( click_button%2 === 0) ? "Dark" : "Light" } Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
